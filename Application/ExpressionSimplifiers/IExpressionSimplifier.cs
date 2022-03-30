0c71244f57c1481aab489e20209146d383fd814f
Michal Fiala <michfiala.it@gmail.com>
2022-03-30 11:14:19 +0200

    Removing simplifiers

--- Application/ExpressionSimplifiers/IExpressionSimplifier.cs
+++
@@ -1,9 +0,0 @@
-using TreeStructure;
-
-namespace Application.ExpressionSimplifiers
-{
-    public interface IExpressionSimplifier
-    {
-         public IExpressionNode TrySimplify(IExpressionNode node);
-    }
-}
